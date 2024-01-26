07050d0ceaa162cd04ddce9e27dd5ffd283449f4
LuisVera1 <101475330+LuisVera1@users.noreply.github.com>
2024-01-25 18:17:16 -0600

    add attribution and fix styles

---
+++ README.md
@@ -0,0 +1,66 @@
+# Frontend Mentor - Blog preview card solution
+
+This is a solution to the [Blog preview card challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/blog-preview-card-ckPaj01IcS).
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See hover state for all interactive elements on the page
+
+### Screenshot
+
+![](./public/Screenshot.png)
+
+### Links
+
+- Solution URL: [Github](https://github.com/LuisVera1/FrontendMentor-Blog-preview-card)
+- Live Site URL: [See solution](https://frontend-mentor-blog-preview-card-chi.vercel.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+
+### What I learned
+
+This is my first project on Frontend Mentor.
+
+For this project I focused on adaptive development and the use of the BEM methodology for a more understandable code.
+
+### Continued development
+
+I am going to work on more projects, and at the same time improve the semantics and optimization.
+
+
+
+## Author
+
+- Website - [Luisvera.dev](https://www.luisvera.dev)
+- Frontend Mentor - [@LuisVera1](https://www.frontendmentor.io/profile/LuisVera1)
+
+
+## Acknowledgments
+
+Thanks to Frontend Mentor.
--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,58 @@ import './App.css'
 
 function App() {
 
+  //add readme
+
   return (
     <main className="main">
+
+      <div className="main__title">
+        <h1>Blog preview card</h1>
+      </div>
+
       <section className="card">
 
         {/* image */}
 
         <figure>
-          <img src="src/assets/illustration-article.svg" alt="blog preview card" className="card__image" />
+          <img src="assets/illustration-article.svg" alt="blog preview card" className="card__image" />
         </figure>
 
         {/* text */}
 
-        <p className="card__category bold">
-          Learning
-        </p>
+        <div className="card__text">
+          <p className="card__category bold">
+            Learning
+          </p>
 
-        <p className="card__published gray">
-          Published{" "}
-          <time className='card__published-time gray' dateTime='2023-12-21'>21 Dec 2023</time>
-        </p>
+          <p className="card__published gray">
+            Published{" "}
+            <time className='card__published-time gray' dateTime='2023-12-21'>21 Dec 2023</time>
+          </p>
 
-        <h1 className="card__title bold">HTML & CSS foundations</h1>
+          <h2 className="card__title bold">HTML & CSS foundations</h2>
 
-        <p className="card__description gray">
-          These languages are the backbone of every website, defining structure, content, and presentation.
-        </p>
+          <p className="card__description gray">
+            These languages are the backbone of every website, defining structure, content, and presentation.
+          </p>
+        </div>
 
         {/* author */}
 
         <div className="author">
           <figure>
-            <img src="src/assets/image-avatar.webp" alt="Greg Hooper" className="author__photo" />
+            <img src="assets/image-avatar.webp" alt="Greg Hooper" className="author__photo" />
           </figure>
 
           <p className="author__name bold">Greg Hooper</p>
         </div>
 
       </section>
+
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="https://github.com/LuisVera1/FrontendMentor-Blog-preview-card">Luis Vera</a>.
+      </div>
     </main>
   )
 }
